3e3cb2e85b66dd9f1697cd08eca9eb5028fadc7a
Sahilw17 <sahilwaghole1784@gmail.com>
2025-08-06 00:27:53 +0530

    dsa

--- Scratch_DSA/3_sum.py
+++ Scratch_DSA/3_sum.py
@@ -1,12 +1,40 @@
-harr=[-1,0,1,2,-1,-4,0]
-mset=set()
+# harr=[-1,0,1,2,-1,-4,0]
+# mset=set()
 
-n=len(harr)
+# n=len(harr)
+# for i in range(0,n):
+#     for j in range(i+1,n):
+#         for k in range(j+1,n):
+#             if((harr[i]+harr[j]+harr[k])==0):
+#                 temp=[harr[i],harr[j],harr[k]]
+#                 temp.sort()
+#                 mset.add(tuple(temp))
+# print( mset)
+
+nums=[-2,-2,-2,-1,-1,-1,0,0,0,2,2,2,2]
+n=len(nums)
+ans=[]
+nums.sort()
 for i in range(0,n):
-    for j in range(i+1,n):
-        for k in range(j+1,n):
-            if((harr[i]+harr[j]+harr[k])==0):
-                temp=[harr[i],harr[j],harr[k]]
-                temp.sort()
-                mset.add(tuple(temp))
-print( mset)
+    if i!=0 and nums[i]==nums[i-1]:
+        continue
+    j=i+1
+    k=n-1
+    while j<k:
+        sum=nums[i]+nums[j]+nums[k]
+        if(sum<0):
+            j+=1
+        elif(sum>0):
+            k-=1
+        else:
+            temp=[nums[i],nums[j],nums[k]]
+            ans.append(temp)
+            j+=1
+            k-=1
+        
+        while j<k and nums[j]==nums[j-1]:
+            j+=1
+        
+        while j<k and nums[k]==nums[k-1]:
+            k-=1
+print(ans)
