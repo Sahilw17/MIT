321384e7aa12815a5651219cc3491e933846996d
Sahilw17 <sahilwaghole1784@gmail.com>
2025-08-16 14:47:14 +0530

    .

---
+++ Java/A1_1.java
@@ -0,0 +1,17 @@
+// wap to add,sub,mul,div two numbers
+
+class A1_1
+{
+    public static void main(String[] args){
+        int a=10;
+        int b=20;
+        int add=a+b;
+        int sub=a-b;
+        int mul=a*b;
+        int div=a/b;
+        System.out.println("Add="+add);
+        System.out.println("Substraction="+sub);
+        System.out.println("Multiplication="+mul);
+        System.out.println("Division="+div);
+    }
+}
