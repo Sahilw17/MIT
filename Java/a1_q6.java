61213122e84acb7bcec153dcd5e3343b436d76d5
Sahilw17 <sahilwaghole1784@gmail.com>
2025-08-16 16:19:45 +0530

    assignment1

---
+++ Java/a1_q6.java
@@ -0,0 +1,15 @@
+public class a1_q6 {
+    public static void main(String[] args) {
+        int a=11;
+        int b=12;
+        if(a<b){
+            System.out.println("b is greater");
+        }
+        else if(a==b){
+            System.out.println("both are equal");
+        }
+        else{
+            System.out.println("a is greater");
+        }
+    }
+}
---
+++ Java/a1_sumofdigits.java
@@ -0,0 +1,11 @@
+public class a1_sumofdigits {
+    public static void main(String[] args) {
+        int a=1234;
+        int sum=0;
+        while(a>0){
+            sum=sum+a%10;
+            a=a/10;
+        }
+        System.out.println(sum);
+    }
+}
