865110a78e5dbe6c6658118c8dcdf4ffbf20a2d2
Sahilw17 <sahilwaghole1784@gmail.com>
2025-08-22 00:27:20 +0530

    Assignment 2

---
+++ Java/Assignment 2/Pattern2.java
@@ -0,0 +1,17 @@
+// A 
+// B B 
+// C C C 
+// D D D D
+
+public class Pattern2 {
+    public static void main(String[] args) {
+        char c='A';
+        for(int i=0;i<4;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print(c+" ");
+            }
+            c++;
+            System.out.println("");
+        }
+    }
+}
