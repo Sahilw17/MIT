b3b14098744b27e86dcd1e7c36612646e8c55fc9
Sahilw17 <sahilwaghole1784@gmail.com>
2025-08-30 14:47:13 +0530

    assignment 4

---
+++ Java/Assignment 4/q2_pack/Num_operations.java
@@ -0,0 +1,10 @@
+package pack2;
+import pack1.numbers;
+public class Num_operations
+{
+	public static void main(String args[]){
+	numbers n=new numbers();
+	n.accept_number();
+	n.operations();
+	}
+}
