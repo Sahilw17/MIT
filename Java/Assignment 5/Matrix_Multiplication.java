4f24dff32acd20d09b4a5422fa8c493a96621c61
Sahilw17 <sahilwaghole1784@gmail.com>
2025-09-01 13:28:38 +0530

    Assignment 5

---
+++ Java/Assignment 5/Matrix_Multiplication.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+class mat
+{
+	int r;
+	int c;
+	int [][] arr;
+	Scanner sc=new Scanner(System.in);
+	mat(int r,int c){
+	this.r= r;
+	this.c= c;
+	arr = new int[r][c];
+	}
+	
+	void create(){
+		System.out.println("Enter the data in the matrix");
+		for (int i=0;i<r ;i++ )
+		{
+			for(int j=0;j<c;j++){
+				arr[i][j]=sc.nextInt();
+			}
+		}
+	}
+	
+	void print(){
+		for (int i=0;i<r ;i++ )
+		{
+			for(int j=0;j<c;j++){
+				System.out.print(arr[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
+	
+	void mult(mat m1,mat m2){
+		for (int i=0;i<r ;i++ )
+		{
+			for (int j=0;j<c ;j++ )
+			{
+				arr[i][j]=m1.arr[i][j] * m2.arr[i][j];
+			}
+		}
+	}
+	
+}
+
+
+class Matrix_Multiplication 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the row and column");
+		int r=sc.nextInt();
+		int c=sc.nextInt();
+		mat m1=new mat(r,c);
+		m1.create();
+		mat m2=new mat(r,c);
+		m2.create();
+		mat m3=new mat(r,c);
+		m3.mult(m1,m2);
+		m3.print();
+	}
+}
